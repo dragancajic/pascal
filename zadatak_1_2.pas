25cc54dcee92d9b5ebbbc4a23c7fc61471f3c03a
Dragan Cajic <cajic_dragan@yahoo.com>
2021-02-28 23:45:29 +0100

    add text in latin

--- zadatak_1_2.pas
+++ zadatak_1_2.pas
@@ -1,14 +1,19 @@
-// Напиши програм који за унесене странице a и b рачуна обим и површину правоугоника.
+// Napisi program koji za unesene stranice a i b racuna obim i povrsinu pravougaonika.
 
 program zadatak_1_2;
 var a, b : integer;
 begin
-	//writeln('Unesite stranice');
-	writeln('Унесите странице правоугаоника');
+	writeln('Unesite stranice pravougaonika:');
 	
 	readln(a, b);
 	//writeln('Obim pravougaonika je : ', 2*(a+b));
 	//writeln('Povrsina pravougaonika je ', a*b);
-	writeln('За странице ', a, ' и ', b, ' обим правоугаоника је ', 2*(a+b), '.');
-	writeln('За странице ', a, ' и ', b, ' површина правоугаоника је ', a*b, '.');
+	writeln('Za stranice ', a, ' i ', b, ' obim pravougaonika je ', 2*(a+b), '.');
+	writeln('Za stranice ', a, ' i ', b, ' povrsina pravougaonika je ', a*b, '.');
+	readln();
 end.
+
+{
+	zadatak_1_2, Pascal - rextester
+	http://rextester.com/JMXGA21101
+}
