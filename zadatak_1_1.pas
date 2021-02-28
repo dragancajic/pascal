da6eab0b4edd0056c1f396ab86e17e53d396258e
Dragan Cajic <cajic_dragan@yahoo.com>
2021-02-28 02:15:57 +0100

    add comment for inputting numbers

--- zadatak_1_1.pas
+++ zadatak_1_1.pas
@@ -4,7 +4,8 @@ program zadatak_1_1;
 var a, b : integer;
 begin
 	writeln('Unesite 2 cijela broja:');
-	{ Brojeve unositi na posebnoj liniji, tj. nakon prvog br. pritisnuti ENTER }
+	{ Brojeve unositi na posebnoj liniji, tj. nakon prvog broja pritisnuti ENTER,
+	  ili nakon prvog broja pritisnuti SPACE taster, pa onda otkucati drugi broj. }
 	readln(a, b);
 	//writeln('Zbir brojeva je: ', a+b);
 	writeln('Zbir brojeva ', a, ' i ', b, ' je ', a+b, '.');
