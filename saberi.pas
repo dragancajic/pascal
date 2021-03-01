dde2b08247b72ac3bd0b5d5f2cfa20a69bbefb83
Dragan Cajic <cajic_dragan@yahoo.com>
2021-03-01 23:00:38 +0100

    add arbitrary integers

---
+++ saberi.pas
@@ -0,0 +1,19 @@
+// Program sabira odabrani broj proizvoljno unesenih sabirka.
+
+PROGRAM saberi;
+
+VAR
+	i, no, sabirak, suma : integer;
+BEGIN
+	write(' unesi broj sabiraka: ');
+	read(no);
+	suma := 0;
+	writeln(' unesi redom sabirke [sabirak, pa ENTER, ...]:');
+	FOR i := 1 TO no DO
+		BEGIN
+			readln(sabirak);
+			suma := suma + sabirak;
+		END;
+	writeln(' zbir je: ', suma);
+	readln;
+END.
